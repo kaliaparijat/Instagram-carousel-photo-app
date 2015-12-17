65e9547bd895bac6092dcbbff8c6b5538781d9d8
Parijat Kalia <kaliaparijat@Parijats-MBP.att.net>
2015-12-17 07:28:54 -0800

    Updated credentials and the use case where the user does not provide authorization

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
 (function() {
 
-  var clientId = 'afe33a52737641a5b122aeca5562b001',
-  redirectUri = 'http://localhost/slack',
+  var clientId = '175205e598af43028e817042fd8e3320',
+  redirectUri = 'http://choreographer-violet-73727.bitballoon.com',
   authUri = 'https://api.instagram.com/oauth/authorize/',
   responseType = 'token',
   endPoint = 'https://api.instagram.com/v1/users/self/media/recent/',// API Endpoint for loading images
   Utils  = App.Helpers,
-  accessToken = Utils.getQueryParamByName('access_token');
-
+  accessToken = Utils.getQueryParamByName('access_token'),
+  accessDenied = Utils.getQueryParamByName('error_reason');
+  if (accessDenied === 'user_denied') {
+    return; // authorization denied for user, app needs user authorization
+  }
   if (accessToken === '') {
       window.location = Utils.getUriWithParams(authUri, {client_id: clientId, redirect_uri: redirectUri, response_type: responseType});
   }
